4149f06e24840be3bd7c93816e157a8dca5e5646
liuayng_msi5600g <liuyang_zero@outlook.com>
2024-07-13 18:06:40 +0800

    修复自动部署脚本

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -2,11 +2,17 @@ $microservicename="pg-jpa-dbpaas-scheduler"
 
 cd ..
 
+mvn clean
+
+mvn package -DskipTests
+
 ssh paas@192.168.1.111 "mkdir -p /home/paas/microservices/$microservicename/"
 
 scp .\target\*.jar paas@192.168.1.111:/home/paas/microservices/$microservicename/
 scp .\scripts\* paas@192.168.1.111:/home/paas/microservices/$microservicename/
 
 ssh paas@192.168.1.111 "cd /home/paas/microservices/$microservicename;chmod u+x *.sh;bash ./restart.sh"
+
+mvn clean
 
 cd scripts
